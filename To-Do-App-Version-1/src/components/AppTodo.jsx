2ae35c3e3818ca1f295b47123bdd6e39a424b5de
“enamul58” <enamullimon@gmail.com>
2023-12-13 14:09:08 +0600

    complete ui of to do app

---
+++ To-Do-App-Version-1/src/components/AppTodo.jsx
@@ -0,0 +1,21 @@
+function AppTodo() {
+  return (
+    <div className="container">
+      <div className="row text-start cm-row">
+        <div className="col-6">
+          <input type="text" placeholder="Enter Todo Here" />
+        </div>
+        <div className="col-4">
+          <input type="date" />
+        </div>
+        <div className="col-2">
+          <button type="button" className="btn btn-success cm-button">
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AppTodo;
